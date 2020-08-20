1f5d468e20bde5adfb011c6859cc92a5a64cab74
Meredith Davis <meredithd23@yahoo.com>
2020-08-19 20:26:59 -0500

    env file

--- sql/connection.js
+++ sql/connection.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql')
+require('dotenv').config();
 
 class Connection {
   constructor() {
@@ -7,9 +8,9 @@ class Connection {
 
       const config = {
         connectionLimit: 100,
-        host: 'localhost',
+        host: 35.238.183.35,
         user: 'root',
-        password: 'password',
+        password: process.env.DB_PASSWORD,
         database: 'admin'
       }
 
